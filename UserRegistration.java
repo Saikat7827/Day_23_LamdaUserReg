97025da6a9d1f67165b7638fb2d273bc10536119
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-10-08 00:21:08 +0530

    Email Reg

--- UserRegistration.java
+++ UserRegistration.java
@@ -13,12 +13,18 @@ public class UserRegistration {
 		Validation isValidFirstName = (pattern, firstName) -> {
 			return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
 		};
-		System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Saiakat"));
+		System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Saikat"));
 
 		Validation isValidLastName = (pattern, lastName) -> {
 			return "Last Name is " + Pattern.compile(pattern).matcher(lastName).matches();
 		};
 		System.out.println(isValidLastName.validate("^[A-Z]{1}[a-z]{2,}$", "Chakraborty"));
+
+		Validation isValidEmail = (pattern, email) -> {
+			return "Email is " + Pattern.compile(pattern).matcher(email).matches();
+		};
+		System.out.println(isValidEmail.validate("^[a-z]+[+-_.]*[a-z]*[@][a-z]+[.][a-z]{2,4}[.]*([a-z]{2})*$",
+				"Saikatchakraborty@gmail.com"));
 	}
 	
 }
